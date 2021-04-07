4ea240243bbff2080b32aa945a42f836e67c519b
gary.archer <gary.archer@GARYWORKMAC.local>
2021-04-07 18:11:43 +0200

    Initial checkin

---
+++ src/main/java/io/curity/example/secureapi/SecureapiApplication.java
@@ -0,0 +1,13 @@
+package io.curity.example.secureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/curity/example/secureapi/SecureapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.curity.example.secureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
